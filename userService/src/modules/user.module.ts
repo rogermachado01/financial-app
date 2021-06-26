e64649843cfec636232b8be5a3b7b362a832c87b
Roger <roger.machado@roit.com.br>
2021-06-25 21:42:46 -0300

    feat(user-service-mongo): started mongodb and mongose config

--- userService/src/modules/user.module.ts
+++ userService/src/modules/user.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { UserResolver } from 'src/graphql/user/user.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { UserResolver } from 'src/graphql/user/user.resolver';
         resolvers: UserResolver,
       }),
     }),
+    MongooseModule.forRoot('mongodb://teste:teste@localhost:27017'),
   ],
 })
 export class UserModule {}
